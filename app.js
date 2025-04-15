4877887eb19df96d4b85f8705f7268642f7fa974
25JT <jhonierlucumimeza@gmail.com>
2025-04-14 23:49:01 -0500

    dev ver0.0.1
    se incia el proyecto se crea conexion con la base de datos en mysql , usando phpmy admin , se crea el servidor node.js , se crea la vista ejs

---
+++ app.js
@@ -0,0 +1,31 @@
+import  conexion  from "./public/conexion.js";
+import express from "express";
+
+
+
+const app = express();
+const port = 3000; 
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.get('/', function (req, res) {
+    res.render('index', { message: 'Datos insertados correctamente.'});
+});
+
+
+//inicia servidor
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
+
+
+// consulta de prueba 
+// conexion.query('SELECT * FROM cliente', (error, results) => {
+//     if (error) throw error;
+//     console.log(results); // Muestra los resultados en la consola
+//   })
---
+++ public/conexion.js
@@ -0,0 +1,26 @@
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+
+
+dotenv.config();
+
+const conexion = mysql.createConnection({
+    host: process.env.host,
+    database:process.env.database,
+    user: process.env.user,
+    password: process.env.password,
+    port: process.env.port
+})
+
+export default conexion;
+
+conexion.connect(function (error){
+if (error){
+    console.log("fallo en la conexion " + error);
+}else{
+    console.log("conexion exitosa");
+    
+}
+});
+    
